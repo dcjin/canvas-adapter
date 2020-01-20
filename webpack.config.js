3995ce5712e13f014a96fde958ae897a3af56ca6
ws <dcjin@Ctrip.com>
2020-01-20 16:30:12 +0800

    modify build output

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,8 @@ module.exports = {
     entry: path.resolve(__dirname, './src/index.ts'),
     output: {
         path: path.resolve(__dirname, './dist/'),
-        filename: 'index.js'
+        filename: 'index.js',
+        libraryTarget: 'commonjs'
     },
     resolve: {
         extensions: ['.js', '.jsx', '.ts', '.tsx']
